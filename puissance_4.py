ac8e81233066121f15a370d97bec9dc856b81082
Sofia T <91561176+uvsq22104096@users.noreply.github.com>
2022-04-23 23:07:25 +0200

    Update puissance_4.py

--- puissance_4.py
+++ puissance_4.py
@@ -17,6 +17,7 @@ DIAMETRE_JETON = 100
 # Variables globals
 configuration = []
 jeuton = []
+joueur = 1
 
 # Fonctions
 
@@ -49,6 +50,38 @@ def affichage_jeuton():
             jeuton.append(canvas.create_oval(j*(DIAMETRE_JETON)+10, i*(DIAMETRE_JETON)+10,
              (j+1)*(DIAMETRE_JETON)-10, (i+1)*(DIAMETRE_JETON)-10,fill = color, outline = color))
 
+            
+def mouvement_jeton(event):
+    """Fonction qui permet de faire tomber le jeton dans la
+    grille jusqu'à toucher le fond ou un autre jeton"""
+    global joueur, configuration
+    x = event.x
+    ligne = -1
+    if x<100:
+        colonne = 0
+    if 100<x<200:
+        colonne = 1
+    if 200<x<300:
+        colonne = 2
+    if 300<x<400:
+        colonne = 3
+    if 400<x<500:
+        colonne = 4
+    if 500<x<600:
+        colonne = 5
+    if 600<x<700:
+        colonne = 6
+    while configuration[ligne][colonne] != 0:
+        ligne -= 1
+    if joueur == 1:
+        configuration[ligne][colonne] = 1
+        joueur = 2
+    elif joueur == 2:
+        configuration[ligne][colonne] = 2
+        joueur = 1
+    affichage_jeuton()
+    
+    
 # Affichage graphique
 
 racine = tk.Tk()
@@ -56,6 +89,7 @@ canvas = tk.Canvas(racine, width=DIAMETRE_JETON*NOMBRE_COLONNE, height=DIAMETRE_
 
 canvas.grid()
 
+canvas.bind('<Button>', mouvement_jeton )
 configuration_initiale()
 affichage_jeuton()
 
